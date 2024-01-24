0ccfe7f3d429834b5b6c843a584a58e400fe01f2
Blacklotus88888 <Song.yang.chen89898@gmail.com>
2024-01-23 22:39:49 -0500

    first cpp tutorial and project

---
+++ LinkList.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+class Node
+{
+public:
+    int val;
+    Node *next;
+
+    Node(int val)
+    {
+        this->val = val;
+        this->next = nullptr;
+    }
+};
+
+class LinkedList
+{
+private:
+    Node *head;
+    Node *tail;
+    int length;
+
+public:
+    LinkedList(int val)
+    {
+        Node *newNode = new Node(val);
+        this->head = newNode;
+        this->tail = newNode;
+        this->length = 1;
+    }
+
+    void printList()
+    {
+        using namespace std;
+        ;
+        Node *ptr = head;
+        while (ptr)
+        {
+            cout << ptr->val << endl;
+            ptr = ptr->next;
+        }
+    }
+
+    void getData() {
+        using namespace std;;
+        cout << head->val << "\n"<< tail->val << "\n" << length << endl;
+    }
+};
+
+
+int main() {
+    LinkedList* ll = new LinkedList(6);
+
+    ll->getData();
+}
